03ef57821eee4c1f583d184f074bea788f530f17
Andrew Febre <afebre1027@gmail.com>
2022-03-16 19:21:41 -0400

    deploying

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:3001/BudgetTracker', {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true
 });
 
 // routes
